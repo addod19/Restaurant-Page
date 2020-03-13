e1d80dd3d4a330bdfe60c8733bf5fe9d47d0e332
Daniel <addodaniellarbi@gmail.com>
2020-03-13 11:01:02 +0000

    setup project with webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+
+function component() {
+    const element = document.createElement('div');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(component());
+console.log('welcome to webpack by daniel!!!');
